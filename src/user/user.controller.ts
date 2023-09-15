3b6c14e5312fc9d0810a53a38987de7dfefd0d0e
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-09-14 23:33:06 -0300

    feat: create users with encrypted id and password

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
+
   @Post()
   async createUser(@Body() createUser: CreateUserDto) {
-    return createUser;
+    return this.userService.createUser(createUser);
   }
 }
