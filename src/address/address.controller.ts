a1862c2705eea6ff7676232104c5419dcceefdda
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-09-20 16:24:18 -0300

    sparkles: feat: create from address module

---
+++ src/address/address.controller.ts
@@ -0,0 +1,26 @@
+import {
+  Body,
+  Param,
+  Post,
+  Controller,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { CreateAddressDto } from './dtos/createAddress.dto';
+import { AddressService } from './address.service';
+import { AddressEntity } from './entities/address.entity';
+
+@Controller('address')
+export class AddressController {
+  constructor(private readonly addressService: AddressService) {}
+
+  @Post('/:userId')
+  @UsePipes(ValidationPipe)
+  async createAddress(
+    @Body() createAddressDto: CreateAddressDto,
+    @Param('userId')
+    userId: number,
+  ): Promise<AddressEntity> {
+    return this.addressService.createAddress(createAddressDto, userId);
+  }
+}
