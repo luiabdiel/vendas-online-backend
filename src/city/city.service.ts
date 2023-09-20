8bcc9d9c4a2bfb2a0fe881b73ca5bf6b6c8915a7
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-09-20 12:50:30 -0300

    :sparkles: feat: create cache module

--- src/city/city.service.ts
+++ src/city/city.service.ts
@@ -1,33 +1,25 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CityEntity } from './entities/city.entity';
-import { CACHE_MANAGER } from '@nestjs/cache-manager';
-import { Cache } from 'cache-manager';
+import { CacheService } from 'src/cache/cache.service';
 
 @Injectable()
 export class CityService {
   constructor(
     @InjectRepository(CityEntity)
     private readonly cityRepository: Repository<CityEntity>,
-    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+
+    private readonly cacheService: CacheService,
   ) {}
 
   async getAllCitiesByStateId(stateId: number): Promise<CityEntity[]> {
-    const citiesCache: CityEntity[] = await this.cacheManager.get(`${stateId}`);
-
-    if (citiesCache) {
-      return citiesCache;
-    }
-
-    const cities = await this.cityRepository.find({
-      where: {
-        stateId: stateId,
-      },
-    });
-
-    await this.cacheManager.set(`${stateId}`, cities);
-
-    return cities;
+    return this.cacheService.getCache<CityEntity[]>(`state_${stateId}`, () =>
+      this.cityRepository.find({
+        where: {
+          stateId: stateId,
+        },
+      }),
+    );
   }
 }
