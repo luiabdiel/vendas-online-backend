d21c3ac7b95f66135c8368d41144c99b20efb850
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-09-19 23:11:26 -0300

    :sparkles: feat: saving data in cache

--- src/city/city.service.ts
+++ src/city/city.service.ts
@@ -1,20 +1,33 @@
-import { Injectable } from '@nestjs/common';
-import { CityEntity } from './entities/city.entity';
+import { Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CityEntity } from './entities/city.entity';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class CityService {
   constructor(
     @InjectRepository(CityEntity)
     private readonly cityRepository: Repository<CityEntity>,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
   ) {}
 
   async getAllCitiesByStateId(stateId: number): Promise<CityEntity[]> {
-    return this.cityRepository.find({
+    const citiesCache: CityEntity[] = await this.cacheManager.get(`${stateId}`);
+
+    if (citiesCache) {
+      return citiesCache;
+    }
+
+    const cities = await this.cityRepository.find({
       where: {
         stateId: stateId,
       },
     });
+
+    await this.cacheManager.set(`${stateId}`, cities);
+
+    return cities;
   }
 }
