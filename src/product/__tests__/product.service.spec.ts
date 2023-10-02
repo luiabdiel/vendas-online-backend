81768370303a06462d300db727d87d213a6682d0
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-10-02 13:12:37 -0300

    :sparkles: feat: create function createProduct

--- src/product/__tests__/product.service.spec.ts
+++ src/product/__tests__/product.service.spec.ts
@@ -4,15 +4,25 @@ import { Repository } from 'typeorm';
 import { ProductEntity } from '../entities/product.entity';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { productMock } from '../__mocks__/product.mock';
+import { createProductMock } from '../__mocks__/create-product.mock';
+import { CategoryService } from '../../category/category.service';
+import { categoryMock } from '../../category/__mocks__/category.mock';
 
 describe('ProductService', () => {
   let service: ProductService;
   let productRepository: Repository<ProductEntity>;
+  let categoryService: CategoryService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         ProductService,
+        {
+          provide: CategoryService,
+          useValue: {
+            findCategoryById: jest.fn().mockResolvedValue(categoryMock),
+          },
+        },
         {
           provide: getRepositoryToken(ProductEntity),
           useValue: {
@@ -27,11 +37,13 @@ describe('ProductService', () => {
     productRepository = module.get<Repository<ProductEntity>>(
       getRepositoryToken(ProductEntity),
     );
+    categoryService = module.get<CategoryService>(CategoryService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
     expect(productRepository).toBeDefined();
+    expect(categoryService).toBeDefined();
   });
 
   it('should return all products', async () => {
@@ -50,5 +62,19 @@ describe('ProductService', () => {
     jest.spyOn(productRepository, 'find').mockRejectedValue(new Error());
 
     expect(service.findAllProduct()).rejects.toThrowError();
+  });
+
+  it('should return product after insert in DB', async () => {
+    const product = await service.createProduct(createProductMock);
+
+    expect(product).toEqual(productMock);
+  });
+
+  it('should return product after insert in DB', async () => {
+    jest
+      .spyOn(categoryService, 'findCategoryById')
+      .mockRejectedValue(new Error());
+
+    expect(service.createProduct(createProductMock)).rejects.toThrowError();
   });
 });
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -2,12 +2,15 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ProductEntity } from './entities/product.entity';
 import { Repository } from 'typeorm';
+import { CategoryService } from '../category/category.service';
+import { CreateProductDto } from './dtos/create-product.dto';
 
 @Injectable()
 export class ProductService {
   constructor(
     @InjectRepository(ProductEntity)
     private readonly productRepository: Repository<ProductEntity>,
+    private readonly categoryService: CategoryService,
   ) {}
 
   async findAllProduct(): Promise<ProductEntity[]> {
@@ -18,5 +21,13 @@ export class ProductService {
     }
 
     return products;
+  }
+
+  async createProduct(createProduct: CreateProductDto): Promise<ProductEntity> {
+    await this.categoryService.findCategoryById(createProduct.categoryId);
+
+    return this.productRepository.save({
+      ...createProduct,
+    });
   }
 }
