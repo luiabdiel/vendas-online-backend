c722ad6fc02d85dc9db8d5d86865240830ea5263
luiabdiel <luiggiabdieldev@gmail.com>
2023-10-29 00:04:07 -0300

    refactor

--- src/migration/1698351407935-create-table-order-product.ts
+++ src/migration/1698351407935-create-table-order-product.ts
@@ -5,20 +5,20 @@ export class CreateTableOrderProduct1698351407935
 {
   public async up(queryRunner: QueryRunner): Promise<void> {
     queryRunner.query(`
-        CREATE TABLE public.order_product (
-          id integer NOT NULL,
-          order_id int NOT NULL,
-          product_id int NOT NULL,
-          amount int NOT NULL,
-          price int NOT NULL,
-          created_at timestamp without time zone DEFAULT now() NOT NULL,
-          updated_at timestamp without time zone DEFAULT now() NOT NULL,
-          primary key (id),
-          foreign key (order_id) references public.order(id),
-          foreign key (product_id) references public.product(id)
-        );
+      CREATE TABLE public.order_product (
+        id integer NOT NULL,
+        order_id int NOT NULL,
+        product_id int NOT NULL,
+        amount int NOT NULL,
+        price int NOT NULL,
+        created_at timestamp without time zone DEFAULT now() NOT NULL,
+        updated_at timestamp without time zone DEFAULT now() NOT NULL,
+        primary key (id),
+        foreign key (order_id) references public.order(id),
+        foreign key (product_id) references public.product(id)
+      );
 
-        CREATE SEQUENCE public.order_product_id_seq
+      CREATE SEQUENCE public.order_product_id_seq
           AS integer
           START WITH 1
           INCREMENT BY 1
@@ -26,10 +26,10 @@ export class CreateTableOrderProduct1698351407935
           NO MAXVALUE
           CACHE 1;
 
-        ALTER SEQUENCE public.order_product_id_seq OWNED BY public.order_product.id;
+      ALTER SEQUENCE public.order_product_id_seq OWNED BY public.order_product.id;
 
-        ALTER TABLE ONLY public.order_product ALTER COLUMN id SET DEFAULT nextval('public.order_product_id_seq'::regclass);
-      `);
+      ALTER TABLE ONLY public.order_product ALTER COLUMN id SET DEFAULT nextval('public.order_product_id_seq'::regclass);
+    `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
