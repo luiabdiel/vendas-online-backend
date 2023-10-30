72159d88634fda3e35d2a0979ab250d0ecbe3374
luiabdiel <luiggiabdieldev@gmail.com>
2023-10-29 23:17:38 -0300

    :sparkles: feat create payment service

--- src/payment/entities/payment-pix.entity.ts
+++ src/payment/entities/payment-pix.entity.ts
@@ -1,5 +1,6 @@
 import { ChildEntity, Column } from 'typeorm';
 import { PaymentEntity } from './payment.entity';
+import { CreateOrderDto } from 'src/order/dtos/create-order.dto';
 
 @ChildEntity()
 export class PaymentPixEntity extends PaymentEntity {
@@ -8,4 +9,16 @@ export class PaymentPixEntity extends PaymentEntity {
 
   @Column({ name: 'date_payment', nullable: false })
   datePayment: Date;
+
+  constructor(
+    statusId: number,
+    price: number,
+    discount: number,
+    finalPrice: number,
+    createOrderDto: CreateOrderDto,
+  ) {
+    super(statusId, price, discount, finalPrice);
+    this.code = createOrderDto?.codePix || '';
+    this.datePayment = new Date(createOrderDto?.datePayment || '');
+  }
 }
