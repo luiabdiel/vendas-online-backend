6196a78af5c2cd18f563c4763274d37ef9d9b62c
luiabdiel <luiggiabdieldev@gmail.com>
2023-11-16 22:19:33 -0300

    :sparkles: feat: create function list all orders

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -10,7 +10,11 @@ import { OrderService } from './order.service';
 import { CreateOrderDto } from './dtos/create-order.dto';
 import { UserId } from '../decorators/user-id.decorator';
 import { OrderEntity } from './entities/order.entity';
+import { Roles } from '../decorators/roles.decorator';
+import { UserType } from '../user/enum/user-type.enum';
+import { ReturnOrderDto } from './dtos/return-order.dto';
 
+@Roles(UserType.Admin, UserType.User)
 @Controller('order')
 export class OrderController {
   constructor(private readonly orderService: OrderService) {}
@@ -27,5 +31,13 @@ export class OrderController {
   @Get()
   async findOrdersByUserId(@UserId() userId: number): Promise<OrderEntity[]> {
     return this.orderService.findOrdersByUserId(userId);
+  }
+
+  @Roles(UserType.Admin)
+  @Get('/all')
+  async findAllOrders(): Promise<ReturnOrderDto[]> {
+    return (await this.orderService.findAllOrders()).map(
+      (order) => new ReturnOrderDto(order),
+    );
   }
 }
